576b58023a2ecddf0986c4298825413478f496cd
AhmedHesham003 <133247705+AhmedHesham003@users.noreply.github.com>
2023-05-11 18:56:45 +0300

    Add files via upload

---
+++ jtxtDisplay.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calcoulator;
+
+/**
+ *
+ * @author hp
+ */
+class jtxtDisplay {
+
+    static void setSize(int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static String getText() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void setText(String iNum) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
